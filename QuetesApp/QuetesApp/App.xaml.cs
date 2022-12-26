8320e31fa832174889231ba230165b231fb82be3
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-12-26 23:01:24 +0600

    Add project files.

---
+++ QuetesApp/QuetesApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using QuetesApp.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuetesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new WelcomePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
